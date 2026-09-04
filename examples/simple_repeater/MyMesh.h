5e8a1f588b0290a755f2ea91acd6a9531cb5bd85
João Brázio <jbrazio@gmail.com>
2026-09-05 00:53:04 +0100

    fix(MyMesh): update LUSOFW_FIRMWARE_VERSION to 2026.9.1-rc5

--- examples/simple_repeater/MyMesh.h
+++ examples/simple_repeater/MyMesh.h
@@ -80,7 +80,7 @@ struct NeighbourInfo {
 #endif
 
 #ifndef LUSOFW_FIRMWARE_VERSION
-  #define LUSOFW_FIRMWARE_VERSION "2026.9.1-rc4"
+  #define LUSOFW_FIRMWARE_VERSION "2026.9.1-rc5"
 #endif
 
 #define FIRMWARE_ROLE "repeater"
